dc32046252b34ea2a06525425e93aa3a48c3c263
Harshit Shukla <harshitcr7777@gamil.com>
2023-01-30 19:52:26 +0530

    Added profile img and done some responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from '../assets/img/port_logo.png';
+import logo from "../assets/img/logo-img.jpeg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -11,8 +11,8 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          <Col size={12} sm={6} className="profile_logo">
+            <img src={logo} alt="profile" className="logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
